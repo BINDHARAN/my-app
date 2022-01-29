99ecbbef8843c9ec4c43ad2bc24c7fca323eec02
BINDHARAN <bindharan885@gmail.com>
2022-01-29 17:29:55 +0530

    Removed code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+
+  let users=[{
+    name:'Thor🐱‍🏍', 
+    profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDD2MZMF0p-HB_qh-hMjy4-6xhO3Qebzf1Fg&usqp=CAU"
+  },
+  {
+   name:'Hulk🐱‍🐉', 
+   profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPJKZRDXYwvMIQi2xFcQ1Cu090V9zZFbRvHA&usqp=CAU" 
+  },
+  {
+     name:'Iron Man😎', 
+     profile:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZNxbBTFWgO5aOD4yqcZGFF2HXh1TxI75n3w&usqp=CAU"
+  }]
+
+
+  
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Welcome name='Bindharan' profile="https://icon-library.com/images/person-image-icon/person-image-icon-6.jpg"/>
+      <Welcome name='Kishore' profile="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV0zscYTnOxutaPDaZ9Un0Ak-y0yR8jw40qA&usqp=CAU" />
+      <Welcome name='Aravind' profile="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL_JlCFnIGX5omgjEjgV9F3sBRq14eTERK9w&usqp=CAU"/> */}
+
+      {users.map((user)=> <Welcome name={user.name} profile={user.profile}/>)
+    }
     </div>
   );
 }
 
-export default App;
+function Welcome({name,profile}) {
+  // const name="Bindharan S !!!"
+  return (
+    <div className="welcome">
+      <img className="img" src={profile} alt="img" />
+      <h2>Hello {name} !!! 💕🎉✨</h2>
+    
+    </div>
+  );
+}
